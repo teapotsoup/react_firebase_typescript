61ad0de4e200a7cce7b325c76156833a6b1bbaa8
teapotsoup <0902ab@naver.com>
2022-07-10 11:05:34 +0900

    before css

--- src/App.tsx
+++ src/App.tsx
@@ -19,39 +19,85 @@ type User = {
   age: number;
 };
 
-type EditProps = {
-  id: string;
-  name: string;
-  age: number;
-  index: number;
+type UlProps = {
+  users: any 
+  setUsers:any
+};
+
+type LiProps = {
+  user: any 
+  index:number
 };
 
 function App() {
-  function Todo() {
-    let [form, setForm] = useState(false);
-    const [newName, setNewName] = useState<string>("");
+  function Ul({users,setUsers}:UlProps){
+    function Li({user, index}:LiProps){
+      let [form, setForm] = useState(false);
+      return(
+            <div key={index}>
+              {" "}
+              <div>Name: {user.name}</div>
+              <div>Age: {user.age}</div>
+              <button
+              className="button"
+                onClick={() => {
+                  increaseAge(user.id, user.age, index);
+                }}
+              >
+                Increase Age
+              </button>
+              <button
+              className="button"
+                onClick={() => {
+                  decreaseAge(user.id, user.age, index);
+                }}
+              >
+                Decrease Age
+              </button>
+              <button
+              className="button"
+                onClick={() => {
+                  deleteUser(user.id);
+                }}
+              >
+                Delete User
+              </button>
+              <div>
+                <button
+                className="button"
+                  onClick={() => {
+                    setForm(form ? false : true);
+                  }}
+                >{`Edit Name & Age`}</button>
+                {form ? (
+                  <form
+                    onSubmit={(e) => updateName(e, user.id, user.age, index)}
+                  >
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        setChangedName(e.target.value);
+                      }}
+                      placeholder={user.name}
+                    />
+                    <input
+                    className="inputtext"
+                      style={{ display: "hidden" }}
+                      type="submit"
+                      value="이름변경"
+                    />
+                  </form>
+                ) : 
+                null}
+              </div>
+            </div>
+      )
+    }
+
     const [changedName, setChangedName] = useState<string>("");
-    const [newAge, setNewAge] = useState<number>(0);
-    const [users, setUsers] = useState<User[]>([]); //users는 [{id:"",name:"",age:},{},...] 형태의 자료형.
+    
 
-    const usersCollectionRef = collection(db, "users"); //users이름의 콜렉션(자료들을 모아놓은 자료구조)
-
-    //create
-    const createUser = async () => {
-      const result = await addDoc(usersCollectionRef, {
-        name: newName,
-        age: newAge,
-      }); //addDoc(저장할 콜렉션, 들어갈 자료형 기입)
-      //result==={converter:null, _key:,firestore:,id,...}등으로 이뤄진 객체
-      const newUser = {
-        id: result.id,
-        name: newName,
-        age: newAge,
-      };
-      await setUsers((users) => [...users, newUser]); //비동기 함수를 사용하는 이유?
-    };
-
-    //update. 버튼 클릭시 인풋창 두개가 생기고 각각 나이, 이름을 바꿀수 있게 설정. 둘중 하나가 비면 빈거는 원래값 그대로 들어가게 설정.
+        //update. 버튼 클릭시 인풋창 두개가 생기고 각각 나이, 이름을 바꿀수 있게 설정. 둘중 하나가 비면 빈거는 원래값 그대로 들어가게 설정.
 
     //**********************UPDATE sector********************************
 
@@ -136,13 +182,47 @@ function App() {
     //delete
     const deleteUser = async (id: string) => {
       const userDoc = doc(db, "users", id);
-      users.map((user) => {
+      users.map((user:any) => {
         if (user.id === id) {
-          setUsers(users.filter((e) => e !== user));
+          setUsers(users.filter((e:any) => e !== user));
         }
       });
       await deleteDoc(userDoc);
     };
+    return(
+      <ul>
+              {users.map((user:any, index: number) => {
+          return (
+            <Li user={user} index={index}></Li>
+          );
+        })}</ul>
+    )
+  }
+  function Todo() {
+
+    const [newName, setNewName] = useState<string>("");
+    
+    const [newAge, setNewAge] = useState<number>(0);
+    const [users, setUsers] = useState<User[]>([]); //users는 [{id:"",name:"",age:},{},...] 형태의 자료형.
+
+    const usersCollectionRef = collection(db, "users"); //users이름의 콜렉션(자료들을 모아놓은 자료구조)
+
+    //create
+    const createUser = async () => {
+      const result = await addDoc(usersCollectionRef, {
+        name: newName,
+        age: newAge,
+      }); //addDoc(저장할 콜렉션, 들어갈 자료형 기입)
+      //result==={converter:null, _key:,firestore:,id,...}등으로 이뤄진 객체
+      const newUser = {
+        id: result.id,
+        name: newName,
+        age: newAge,
+      };
+      await setUsers((users) => [...users, newUser]); //비동기 함수를 사용하는 이유?
+    };
+
+
 
     //얘가 read다
     useEffect(() => {
@@ -176,112 +256,12 @@ function App() {
           }}
         />
         <button className="button" onClick={createUser}>Create User</button>
-        {users.map((user, index: number) => {
-          return (
-            <div key={index}>
-              {" "}
-              <div>Name: {user.name}</div>
-              <div>Age: {user.age}</div>
-              <button
-                onClick={() => {
-                  increaseAge(user.id, user.age, index);
-                }}
-              >
-                Increase Age
-              </button>
-              <button
-                onClick={() => {
-                  decreaseAge(user.id, user.age, index);
-                }}
-              >
-                Decrease Age
-              </button>
-              <button
-                onClick={() => {
-                  deleteUser(user.id);
-                }}
-              >
-                Delete User
-              </button>
-              <div>
-                <button
-                  onClick={() => {
-                    setForm(form ? false : true);
-                  }}
-                >{`Edit Name & Age`}</button>
-                {form ? (
-                  <form
-                    onSubmit={(e) => updateName(e, user.id, user.age, index)}
-                  >
-                    <input
-                      type="text"
-                      onChange={(e) => {
-                        setChangedName(e.target.value);
-                      }}
-                      placeholder={user.name}
-                    />
-                    <input
-                      style={{ display: "hidden" }}
-                      type="submit"
-                      value="이름변경"
-                    />
-                  </form>
-                ) : //<Editform id={user.id} age={user.age} index={index} name={user.name}/>
-                null}
-              </div>
-            </div>
-          );
-        })}
+        {users?<Ul users={users} setUsers={setUsers}/>:null}
+
+
       </div>
     );
   }
-
-  // function Editform({id,age,index,name}:EditProps){
-  //   return(
-  //     <form
-  //     onSubmit={async (e) => {
-  //       e.preventDefault();
-  //       const userDoc = doc(db, "users", id);
-
-  //       const newFields = { name: changedName };
-
-  //       await onSnapshot(doc(db, "users", id), (doc) => {
-  //         // let getName = doc.data()!.name;
-  //         const newUser = {
-  //           id,
-  //           name: changedName,
-  //           age: age,
-  //         };
-
-  //         // setUsers([...users, newUser]);
-  //         setUsers([
-  //           ...users.slice(0, index),
-  //           newUser,
-  //           ...users.slice(index + 1, users.length),
-  //         ]);
-  //       });
-
-  //       await updateDoc(userDoc, newFields);
-  //     }}//updateName(e, id, age, index)
-  //   >
-  //     <input
-  //       type="text"
-  //       onChange={ (e) => { //온체인지가 하자가 있다.
-  //         console.log(e.target.value)
-  //         if(e.target.value!==""){
-  //           setChangedName(e.target.value);
-  //         }
-
-  //       }}
-  //       placeholder={name}
-  //     />
-  //     <input
-  //       type="submit"
-  //       value="이름변경"
-  //     />
-  //   </form>
-  //   )
-  // }
 
   return (
     <div className="App bigcase">
